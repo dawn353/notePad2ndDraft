9d9b0f1bedc3e6bde6502846862472287c15c3de
“YOUR <annichiarico.d@gmail.com>
2018-06-28 15:19:32 -0400

    Demo ready

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
-
-function Newnote (title, textEdit) {
-  return { 
-   title: title,
-   textEdit: textEdit
-  }
-}
-
-// test button 
-Newnote.prototype.createBtn = function(){
-  var input = document.createElement('input');
-  input.value = 'Save';
-  input.type = 'button';
-}
-
-// function newCard() {
-//   var newCard = new Newnote();
-  
-//   // create a node that creates a List item
-//   var liNode = document.createElement('li');
-//   // create a node that holds all of the card HTML;
-//   var htmlCard = document.createElement('')
-  
-//   node.appendChild(htmlCard);
-//   // newCard.createBtn(document.body); 
-                                  
-// }
+var loopCard = [];
+var count = 1;
 
 function newCard(){
-  $("body").append(` <div class="col-sm-4"> 
-         <div class="panel panel-info">
-           <div class="panel-heading">Note</div>
-            <div class="note">
-              <strong>
-                <BR/>
-                  <TEXTAREA style="WIDTH: 259px; HEIGHT: 30px" rows="3" wrap="virtual" cols=48></TEXTAREA><BR></strong><BR/>         
-                  <TEXTAREA style="WIDTH: 259px; HEIGHT: 182px" id="output" name="output" rows="9" wrap="virtual" cols=48></TEXTAREA>
-                <BR>
-            </div>
-          </div>
-        </div>`);
+
+ var card = $("#myCards").append(` <li class='eachCard'><div class="col-sm-4">
+        <div class="panel panel-info">
+          <div class="panel-heading">Note</div>
+           <div class="note">
+             <strong>
+               <BR/>
+                 <TEXTAREA style="WIDTH: 259px; HEIGHT: 30px" rows="3" wrap="virtual" cols=48></TEXTAREA><BR></strong><BR/>        
+                 <TEXTAREA style="WIDTH: 259px; HEIGHT: 182px" id="output" name="output" rows="9" wrap="virtual" cols=48></TEXTAREA>
+               <BR>
+           </div>
+         </div>
+       </div></li>`);
+
+ loopCard.push(card);
+ document.getElementById('numberOfNotes').innerHTML = loopCard.length;
+}
+
+
+function removeCard(){
+ $('button').click(function() {
+   $('li').remove('.eachCard');
+   document.getElementById('numberOfNotes').innerHTML = count--;
+ })
 }
